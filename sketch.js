be3374e81ef074b475a0f4fb3620d961f8c4cf51
Wojtic <vojtacerny012@gmail.com>
2024-12-16 22:08:35 +0100

    Salkooo

--- sketch.js
+++ sketch.js
@@ -7,13 +7,15 @@ const ROW_HEIGHT = D * 1.5;
 
 const [W, H] = [LENGTH * D + 3 * D, ROWS * ROW_HEIGHT + D];
 const [OFFSET_X, OFFSET_Y] = [2 * D, D];
-const COLOURS = [
+/*const COLOURS = [
   [252, 186, 3],
   [26, 35, 189],
   [235, 19, 37],
   [255, 255, 255],
   [20, 219, 209],
-];
+];*/
+
+const COLOURS = ["#FCBA03", "#1A23BD", "#EB1325", "#FFFFFF", "#14DBD1"];
 
 const GAME = [];
 
@@ -25,8 +27,14 @@ for (let i = 0; i < ROWS; i++) {
   for (let j = 0; j < BEADS; j++) GAME[i].push(j);
 }
 
+let kocka;
+
+function preload() {
+  kocka = loadImage("./kocka.png");
+}
+
 function setup() {
-  createCanvas(W, H);
+  createCanvas(1300, 1400);
 }
 
 function flickBeads() {
@@ -37,7 +45,7 @@ function flickBeads() {
 }
 
 function mousePressed() {
-  let inputX = touches.length > 0 ? touches[0].x : mouseX;
+  let inputX = (touches.length > 0 ? touches[0].x : mouseX) - 200;
   let inputY = touches.length > 0 ? touches[0].y : mouseY;
   let clickedRow = floor((inputY - OFFSET_Y + ROW_HEIGHT / 2) / ROW_HEIGHT);
   if (clickedRow < 0 || clickedRow >= ROWS) return;
@@ -70,15 +78,15 @@ function touchStarted() {
 
 function draw() {
   background(255);
-  translate(OFFSET_X, OFFSET_Y);
+  translate(OFFSET_X + 200, OFFSET_Y);
   if (selectedIndex !== null) {
-    if ((mouseX - OFFSET_X) / D > LENGTH - BEADS + selectedIndex) {
+    if ((mouseX - 200 - OFFSET_X) / D > LENGTH - BEADS + selectedIndex) {
       for (let i = selectedIndex; i < BEADS; i++)
         GAME[selectedRow][i] = LENGTH - BEADS + i;
-    } else if ((mouseX - OFFSET_X) / D < selectedIndex) {
+    } else if ((mouseX - 200 - OFFSET_X) / D < selectedIndex) {
       for (let i = 0; i <= selectedIndex; i++) GAME[selectedRow][i] = i;
     } else {
-      GAME[selectedRow][selectedIndex] = (mouseX - OFFSET_X) / D;
+      GAME[selectedRow][selectedIndex] = (mouseX - 200 - OFFSET_X) / D;
       for (let i = selectedIndex + 1; i < BEADS; i++)
         if (GAME[selectedRow][i] - GAME[selectedRow][i - 1] < 1)
           GAME[selectedRow][i] = GAME[selectedRow][i - 1] + 1;
@@ -92,6 +100,10 @@ function draw() {
 
   strokeWeight(0);
   fill(160, 82, 45);
+  let gradient = drawingContext.createLinearGradient(0, 0, width, height);
+  gradient.addColorStop(0, "#09ff00");
+  gradient.addColorStop(1, "#2605ff");
+  drawingContext.fillStyle = gradient;
   rect(-OFFSET_X + D / 2, -OFFSET_Y, D, ROWS * ROW_HEIGHT + D, D, D, 0, 0);
   rect(LENGTH * D - D / 2, -OFFSET_Y, D, ROWS * ROW_HEIGHT + D, D, D, 0, 0);
   stroke(0);
@@ -99,11 +111,16 @@ function draw() {
   for (let i = 0; i < ROWS; i++)
     line(-D / 2, i * ROW_HEIGHT, LENGTH * D - D / 2, i * ROW_HEIGHT);
   drawBeads();
+  image(kocka, -300, 700, 1300, 600);
 }
 
 function drawBeads() {
   for (let i = 0; i < ROWS; i++) {
-    fill(COLOURS[i % COLOURS.length]);
+    //fill(COLOURS[i % COLOURS.length]);
+    let gradient = drawingContext.createLinearGradient(200, 0, 700, 0);
+    gradient.addColorStop(0, COLOURS[i % COLOURS.length]);
+    gradient.addColorStop(1, COLOURS[(i + 1) % COLOURS.length]);
+    drawingContext.fillStyle = gradient;
     for (let j = 0; j < BEADS; j++) {
       strokeWeight(2);
       circle(GAME[i][j] * D, i * ROW_HEIGHT, D - 2);
